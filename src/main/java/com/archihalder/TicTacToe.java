986d29411dce9c7ba83e28c831f1f500b9dae2c8
Archi Halder <archih812@gmail.com>
2026-01-07 15:42:32 +0530

    handle invalid input without changing turns

--- src/main/java/com/archihalder/TicTacToe.java
+++ src/main/java/com/archihalder/TicTacToe.java
@@ -4,24 +4,36 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        char[] moves = {'O', 'X'};
+        char[] moves = { 'O', 'X' };
         Board board = new Board();
         Scanner sc = new Scanner(System.in);
         int choice = 0;
-        while(true) {
+        while (true) {
             board.displayBoard();
             System.out.println("Player with move: " + moves[choice]);
             System.out.print("Enter your position: ");
-            int pos = sc.nextInt();
-            board.insertMove(moves[choice], pos);
-            if (board.checkStatus() == 'D') {
-                System.out.println("The game ended in draw");
-                break;
-            } else if (board.checkStatus() != 'C') {
-                System.out.println("Player: " + moves[choice] + " won the game");
-                break;
+
+            int pos = 0;
+            try {
+                pos = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid position.");
+                continue;
             }
-            choice = choice == 0 ? 1 : 0;
+
+            if (!board.insertMove(moves[choice], pos))
+                continue;
+            else {
+                if (board.checkStatus() == 'D') {
+                    System.out.println("The game ended in draw");
+                    break;
+                } else if (board.checkStatus() != 'C') {
+                    System.out.println("Player: " + moves[choice] + " won the game");
+                    break;
+                }
+                choice = choice == 0 ? 1 : 0;
+            }
+
         }
         sc.close();
     }
